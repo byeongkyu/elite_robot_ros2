1a24f382dabfb440ee00fc0faa520fbcba14cfb1
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2023-08-23 11:27:13 +0900

    add moveit package

---
+++ elite_robot_moveit_config/launch/demo.launch.py
@@ -0,0 +1,13 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_demo_launch
+
+
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder("cs66_robot", package_name="elite_robot_moveit_config")
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner", "stomp"]
+        )
+        .to_moveit_configs()
+    )
+    return generate_demo_launch(moveit_config)
